3626a59b272bcb8c6b30416adbac0861bcba76b4
bigmandarine <99091870+bigmandarine@users.noreply.github.com>
2022-09-05 11:57:56 +0000

    Deploying to gh-pages from @ bigmandarine/goit-js-hw-09@9bd795796f48705fc524065a0bf963327490c893 🚀

---
+++ 01-color-switcher.da75c6f6.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};let e=null;t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(()=>{e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(()=>{clearInterval(e),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.da75c6f6.js.map
