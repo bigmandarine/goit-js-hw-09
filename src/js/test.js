b79be7bcfe36b8dad27c3a747bca9bcb27bd0dbe
Dima Kuzko <bigmandarine1991@gmail.com>
2022-09-08 16:57:58 +0300

    ready promise

--- src/js/test.js
+++ src/js/test.js
@@ -1,12 +1,3 @@
-import Notiflix from 'notiflix';
-
-const refs = {
-  delay: document.querySelector('[name="delay"]'),
-  step: document.querySelector('[name="step"]'),
-  amount: document.querySelector('[name="amount"]'),
-  btnCreatePromise: document.querySelector('[type="submit"]'),
-};
-
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -22,9 +13,9 @@ function createPromise(position, delay) {
 }
 
 function onClickBtnCreatePromise(evt) {
+  let firstDelay = Number(refs.delay.value);
+  let delayStep = Number(refs.step.value);
   evt.preventDefault();
-  const firstDelay = Number(refs.delay.value);
-  const delayStep = Number(refs.step.value);
   for (let i = 0; i < refs.amount.value; i += 1) {
     createPromise(i + 1, firstDelay + i * delayStep)
       .then(({ position, delay }) => {
